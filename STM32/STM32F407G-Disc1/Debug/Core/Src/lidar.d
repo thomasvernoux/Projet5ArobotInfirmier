ed3c02e204facde68dc91b8fc41f59c0943ebdc8
totoaker <71326141+thomasvnx@users.noreply.github.com>
2021-10-17 14:11:41 +0200

    v les 2 uart de la disco marchent mais le lidar ne repond pas

--- STM32/STM32F407G-Disc1/Debug/Core/Src/lidar.d
+++ STM32/STM32F407G-Disc1/Debug/Core/Src/lidar.d
@@ -25,7 +25,8 @@ Core/Src/lidar.o: ../Core/Src/lidar.c ../Core/Inc/lidar.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ ../Core/Inc/usart.h
 
 ../Core/Inc/lidar.h:
 
@@ -84,3 +85,5 @@ Core/Src/lidar.o: ../Core/Src/lidar.c ../Core/Inc/lidar.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+../Core/Inc/usart.h:
